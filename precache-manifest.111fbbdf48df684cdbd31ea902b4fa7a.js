9e8e902416502e0122d0766e890bb019c0126706
Beroni <beroni.magalhaes@fortbrasil.com.br>
2020-05-16 11:40:18 -0300

    Updates

---
+++ precache-manifest.111fbbdf48df684cdbd31ea902b4fa7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b6a35f964aefd6b6c1a1bb960488da",
+    "url": "/beroni.dev/index.html"
+  },
+  {
+    "revision": "9e576895dce2a3a65067",
+    "url": "/beroni.dev/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0b1c1b8b93d78535d018",
+    "url": "/beroni.dev/static/js/2.15931a8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/beroni.dev/static/js/2.15931a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e576895dce2a3a65067",
+    "url": "/beroni.dev/static/js/main.a9feaafb.chunk.js"
+  },
+  {
+    "revision": "03f5710ca9b98ae3bbb6",
+    "url": "/beroni.dev/static/js/runtime-main.0e9ce0d0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/beroni.dev/static/media/logo.5d5d9eef.svg"
+  }
+]);
