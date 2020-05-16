eddb425966c336b6bd8be8bf500c059ccf2066ca
Beroni <beroni.magalhaes@fortbrasil.com.br>
2020-05-16 13:21:59 -0300

    Updates

---
+++ precache-manifest.e31e7e3f0b5cae2ec2cb8790759ddb5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "042c378d0808f6c1ce5aef0fb2a3b2b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76e2c0f0b4e1fa461aea",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0b1c1b8b93d78535d018",
+    "url": "/static/js/2.15931a8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.15931a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76e2c0f0b4e1fa461aea",
+    "url": "/static/js/main.a10bf9ea.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/beroni.dev/precache-manifest.111fbbdf48df684cdbd31ea902b4fa7a.js"
+  "/precache-manifest.e31e7e3f0b5cae2ec2cb8790759ddb5a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/beroni.dev/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
